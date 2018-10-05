da11aa5c5d95679d22ab7cefd01d9f1736734921
Mollie Ogaz <mhogaz@ucdavis.edu>
2018-10-05 11:30:44 -0700

    how to download from excel with readxl package

--- testcode.r
+++ testcode.r
@@ -1,1 +1,6 @@
 obj <- read.csv("data/lmtest.csv")
+
+install.packages("readxl")
+library(readxl)
+
+#go to environment and then choose import datset and from excel file on the drop down menu to import 
