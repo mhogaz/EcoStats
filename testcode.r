dfcda92ec228cb3e6afd9ee6df5778f5e60af5db
Lily Tomkovic <latomkovic@ucdavis.edu>
2018-10-05 11:33:52 -0700

    moved install around

--- testcode.r
+++ testcode.r
@@ -1,6 +1,6 @@
 obj <- read.csv("data/lmtest.csv")
 
-install.packages("readxl")
 library(readxl)
 
 #go to environment and then choose import datset and from excel file on the drop down menu to import 
+install.packages("readxl")
