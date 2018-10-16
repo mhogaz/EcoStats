3b1b1eaae5a61e95d3118bbe1a7df928c79fcadf
Lily Tomkovic <latomkovic@ucdavis.edu>
2018-10-16 14:08:32 -0700

    added files for 10/16 and Rmd file from 10/2

---
+++ scripts/Session4_HT_MLR.R
@@ -0,0 +1,14 @@
+#Import data - text file (base) - make sure that it will import properly
+
+library(car)  # includes vif
+
+df <- mercury
+
+
+#part a - liver mass
+
+
+#part b - tissue mercury concentration
+
+
+#part c - growth
